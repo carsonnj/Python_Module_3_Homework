2bb1d11ab197ff9a2424d0ad96f220b35f2f15e8
carsonnj <144236409+carsonnj@users.noreply.github.com>
2023-10-03 21:15:16 -0400

    m- 10.3.23

---
+++ PyPoll Analysis/Output.py
@@ -0,0 +1,71 @@
+import os
+import csv
+
+electionCsv = os.path.join(os.getcwd(),'resources','election_data.csv')
+
+totalVotes = 0 # total rows (not including the header is the total of votes)
+
+# empty dictionary to catch votes should be:
+# votesPerCandidate = {
+#   "candidate_one": votes as int
+# }
+votesPerCandidate = {}
+
+# open up election_data
+with open(electionCsv, newline='') as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # print(csvreader)
+
+    # Read the header row first
+    csv_header = next(csvreader)
+    # print(f"CSV Header: {csv_header}")
+
+    # Read each row of data after the header
+    for row in csvreader:
+        totalVotes += 1
+        if row[2] not in votesPerCandidate:
+            votesPerCandidate[row[2]] = 1
+        else:
+            votesPerCandidate[row[2]] += 1   
+        
+        
+
+
+print("Election Results")
+print("-------------------------")
+print("Total Votes: " + str(totalVotes))
+print("-------------------------")
+
+for candidate, votes in votesPerCandidate.items():
+    print(candidate + ": " + "{:.3%}".format(votes/totalVotes) + "   (" +  str(votes) + ")")
+    
+print("-------------------------") 
+
+winner = max(votesPerCandidate, key=votesPerCandidate.get)
+
+print(f"Winner: {winner}")
+
+# now write this to an output file
+
+f = open("election_results.txt", "w")
+f.write("Election Results")
+f.write('\n')
+f.write("-------------------------")
+f.write('\n')
+f.write("Total Votes: " + str(totalVotes))
+f.write('\n')
+f.write("-------------------------")
+f.write('\n')
+
+for candidate, votes in votesPerCandidate.items():
+    f.write(candidate + ": " + "{:.3%}".format(votes/totalVotes) + "   (" +  str(votes) + ")")
+    f.write('\n')
+  
+f.write("-------------------------") 
+f.write('\n')
+f.write(f"Winner: {winner}")
+f.write('\n')
+
